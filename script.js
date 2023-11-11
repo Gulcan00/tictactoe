a722af7ec7484c8eba6aed1263dca269804433dd
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-11-11 19:06:37 +0200

    Refactor game status and winner handling

--- script.js
+++ script.js
@@ -60,41 +60,41 @@ function createGameboardController(
         }
     ];
 
-    let winner;
+    let gameStatus = {status: "ready", winner: null};
 
-    const getWinner = () => winner;
+    const getGameStatus = () => gameStatus;
 
     const checkWin = (board) => {
         const boardIsFull = board.every(row => row.every(cell => cell.getMark() !== ''));
         if (boardIsFull) {
-            return "tie";
+            return {status: "tie", winner: null}
         }
 
         //Check horizontal
         for (let row = 0; row < 3; ++row) {
             if (board[row][0].getMark() !== '' && board[row][0].getMark() === board[row][1].getMark() && board[row][1].getMark() === board[row][2].getMark()) {
-                return board[row][0];
+                return {status: "won", winner: board[row][0]};
             }
         }
 
         //Check vertical
         for (let col = 0; col < 3; ++col) {
             if (board[0][col].getMark() !== '' && board[0][col].getMark() === board[1][col].getMark() && board[1][col].getMark() === board[2][col].getMark()) {
-                return board[0][col];
+                return {status: "won", winner: board[0][col]}
             }
         }
 
         //Check diagonal
         if (board[0][0].getMark() !== '' && board[0][0].getMark() === board[1][1].getMark() && board[1][1].getMark() === board[2][2].getMark()) {
-            return board[0][0];
+            return {status: "won", winner: board[0][0]}
         }
 
         //Check anti diagonal
         if (board[0][2].getMark() !== '' && board[0][2].getMark() === board[1][1].getMark() && board[1][1].getMark() === board[2][0].getMark()) {
-            return board[0][2];
+            return {status: "won", winner: board[0][2]};
         }
 
-        return null;
+        return {status: "ongoing", winner: null}
 
     }
 
@@ -114,15 +114,14 @@ function createGameboardController(
     const playRound = (row, col) => {
         board.setMark(row, col, activePlayer.marker);
 
-        winner = checkWin(board.getBoard());
+        gameStatus = checkWin(board.getBoard());
 
-        if (winner) {
-            if (winner === "tie") {
+        if (gameStatus.status !== "ongoing") {
+            if (gameStatus.status === "tie") {
                 console.log("It's a tie!");
             } else {
             console.log("win!");
-            const player = players.find(player => player.marker === winner.getMark());
-            console.log(player.name);
+            console.log(activePlayer.name);
             }
             board.printBoard();
             return;
@@ -137,7 +136,7 @@ function createGameboardController(
     return {
         getActivePlayer,
         playRound,
-        getWinner,
+        getGameStatus,
         getBoard: board.getBoard
     }
 }
@@ -159,9 +158,12 @@ function createGameboardController(
             boardDiv.appendChild(button);
         }));
 
-        if (game.getWinner()) {
+        if (game.getGameStatus().status === "won") {
             boardDiv.style.display = "none";
-            winnerDiv.innerText = game.getWinner().getMark() + " WON!!!!";
+            winnerDiv.innerText = game.getActivePlayer().name + " WON!!!!";
+        } else if (game.getGameStatus().status === "tie") {
+            boardDiv.style.display = "none";
+            winnerDiv.innerText = "tie!!!";
         }
     }
 
